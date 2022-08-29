1dc6895087619a92d80818bc0ead0d7999144f0b
MariaEugenia <eugeniialima@gmail.com>
2022-08-29 18:40:51 -0300

    Exercicio pronto

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let nome = prompt("Qual é o seu nome?")
+let idade = prompt("Qual é sua idade?")
+
+// console.log(typeof nome, typeof idade) //Idade também é string 
+
+
+// console.log("Olá ", nome, ",você tem", idade, "anos")
+console.log(`Olá ${nome}, você tem ${idade} anos`)
